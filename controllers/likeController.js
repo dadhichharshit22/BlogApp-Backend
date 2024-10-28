69b0f66ed9a81ff9c8fb2c467a469443e33c9aed
dadhichharshit22 <dadhich.harshit222002@gmail.com>
2024-10-28 19:13:57 +0530

    Initial commit

---
+++ controllers/likeController.js
@@ -0,0 +1,61 @@
+// import Models
+const Post = require("../models/postModel");
+const Like = require("../models/likeModel");
+
+// like a post
+
+exports.likePost = async (req,res)=>{
+    try{
+     const {post,user} = req.body;
+     const like = new Like({
+        post,user,
+     });
+     const savedLike = await like.save();
+     // update the post collection basis on this 
+     const updatedPost = await Post.findByIdAndUpdate(post,{$push:{likes:savedLike._id}},{new:true}).populate("likes").exec();
+
+     res.json({
+        post:updatedPost,
+     });
+    }
+    catch(error){
+        return res.status(400).json({
+            error:"Error While Liking Post",
+          });
+    }
+}
+
+
+// unlike a post
+exports.unlikePost = async(req,res)=>{
+    try{
+     const {post,like} = req.body;
+     // find and delete the like collection me se
+     const deletedLike = await Like.findOneAndDelete({post:post,_id:like});
+
+     // update the post collection
+     const updatedPost = await Post.findByIdAndUpdate(post,{$pull:{likes:deletedLike._id}},{new:true});
+
+
+     res.json({
+        post:updatedPost,
+     });
+    }
+    catch(error){
+        return res.status(400).json({
+            error:"Error While unLiking Post",
+          });
+    }
+}
+
+
+
+
+
+
+
+
+// dummy link for testing
+// exports.dummyLink = (req,res) =>{
+//     res.send("This is Your Dummy Page");
+// };
